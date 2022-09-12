33542a5f5c208b268f5d5e22490de649a552e7c0
chidimma <ochidimma447@gmail.com>
2022-09-12 08:56:22 -0700

    read

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+char c = 'z';
+for (; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
